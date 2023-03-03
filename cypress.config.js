a5d662903fa919d1891bf1b3967a1e37b284e870
Rafael Paiva Veiga <rafael.veiga@gkt.grupokabum.com.br>
2023-03-03 11:05:52 -0300

    Ajustes

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: "u928gc",
+    baseUrl:"https://www.stg.kabum.com.br",
+    viewportWidth: 1440,
+    viewportHeight: 720,
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  },
+});
